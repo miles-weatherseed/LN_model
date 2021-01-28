8b3a64456b7de5574ee33377b06b03aa9f449b8e
mweatherseed <mweatherseed@DESKTOP-82ESI4N>
2021-01-28 03:49:12 +0000

    first commit

---
+++ src/functions.rs
@@ -0,0 +1,163 @@
+use rand::Rng;
+
+pub mod functions {
+    use rand::StdRng;
+    use rand::distributions::{Distribution, Gamma, Normal};
+    use statrs::distribution::{Binomial, Univariate, Distribution as OtherDistribution};
+    pub struct DCell {
+        pub x: f64,
+        pub y: f64,
+        pub z: f64,
+        pub cog_ag_ratio: f64,
+        pub prob_activation: f64,
+        pub time_antigen_count_last_updated: f64,
+    }
+    pub struct TCell {
+        pub x: f64,
+        pub y: f64,
+        pub z: f64,
+        pub vx: f64,
+        pub vy: f64,
+        pub vz: f64,
+        pub failed_dc_contact: Vec<i32>,
+        pub interactions: u32,
+    }
+    pub fn add_one(x: u32) -> u32{
+        x + 1
+    }
+    pub fn magnitude(a: f64, b:f64, c:f64) -> f64 {
+        f64::sqrt(f64::powf(a, 2.0) + f64::powf(b, 2.0) + f64::powf(c, 2.0))
+    }
+    pub fn sq_magnitude(a: f64, b:f64, c:f64) -> f64 {
+        f64::powf(a, 2.0) + f64::powf(b, 2.0) + f64::powf(c, 2.0)
+    }
+    pub fn generate_coords(radius: f64) ->  (f64, f64, f64){
+        (radius*(2.0*rand::random::<f64>() - 1.0), radius*(2.0*rand::random::<f64>() - 1.0), radius*(2.0*rand::random::<f64>() - 1.0))
+    }
+    pub fn outside_sphere(x: f64, y: f64, z: f64, radius: f64) -> bool {
+        magnitude(x, y, z) > radius
+    }
+    pub fn set_coordinates(x: f64, y: f64, z: f64, radius: f64, cell_side: f64) -> (i32, i32, i32){
+        (((x + radius)/cell_side) as i32, ((y + radius)/cell_side) as i32, ((z + radius)/cell_side) as i32)
+    }
+    pub fn threed_to_oned(coord_x: i32, coord_y: i32, coord_z: i32, num_positions: u32) -> i32 {
+        return num_positions as i32*num_positions as i32*coord_x + num_positions as i32*coord_y + coord_z
+    }
+    pub fn check_contact_with_dendrites_t(x: f64, y: f64, z: f64, radius: f64, cell_side: f64, num_positions: u32, occupied_positions: &Vec<Vec<u32>>, d_cell_list: &Vec<DCell>, contact_radius: f64, failed_interaction_id: &Vec<i32>) -> (i32, bool){
+        let (coord_x, coord_y, coord_z) = set_coordinates(x, y, z, radius, cell_side);
+        let ref nearby_dcs = &occupied_positions[threed_to_oned(coord_x, coord_y, coord_z, num_positions) as usize];
+        let mut d_cell_num: i32 = -1; let mut in_contact: bool = false;
+        for &nb_dc in *nearby_dcs {
+            if failed_interaction_id.iter().any(|&i| i== nb_dc as i32) {continue;}
+            let dendx: f64 = d_cell_list[nb_dc as usize].x; let dendy: f64 = d_cell_list[nb_dc as usize].y; let dendz: f64 = d_cell_list[nb_dc as usize].z;
+            if sq_magnitude(x - dendx, y - dendy, z - dendz) <= contact_radius*contact_radius {
+                in_contact = true; d_cell_num = nb_dc as i32; break;
+            }
+        }
+        return (d_cell_num, in_contact)
+    }
+    pub fn check_contact_with_dendrites_d(x: f64, y: f64, z: f64, radius: f64, cell_side: f64, num_positions: u32, occupied_positions: &Vec<Vec<u32>>, d_cell_list: &Vec<DCell>, contact_radius: f64) -> (i32, bool) {
+        let (coord_x, coord_y, coord_z) = set_coordinates(x, y, z, radius, cell_side);
+        let ref nearby_dcs = &occupied_positions[threed_to_oned(coord_x, coord_y, coord_z, num_positions) as usize];
+        let mut d_cell_num: i32 = -1; let mut in_contact: bool = false;
+        for &nb_dc in *nearby_dcs {
+            let dendx: f64 = d_cell_list[nb_dc as usize].x; let dendy: f64 = d_cell_list[nb_dc as usize].y; let dendz: f64 = d_cell_list[nb_dc as usize].z;
+            if sq_magnitude(x - dendx, y - dendy, z - dendz) <= contact_radius*contact_radius {
+                in_contact = true; d_cell_num = nb_dc as i32; break;
+            }
+        }
+        return (d_cell_num, in_contact)
+    }
+    pub fn place_t_cells(radius: f64, cell_side:f64, num_positions: u32, preoccupied_positions: &Vec<Vec<u32>>, d_cell_list: &Vec<DCell>, contact_radius: f64) -> (f64, f64, f64){
+        let mut in_contact: bool = false;
+        let mut d_cell_num: i32 = -1;
+        let mut x: f64; let mut y: f64; let mut z: f64;
+        let (x, y, z) = loop {
+            let (x, y, z) = generate_coords(radius);
+            if outside_sphere(x, y, z, radius) {
+                continue;
+            }
+            let (d_cell_num, in_contact) = check_contact_with_dendrites_t(x, y, z, radius, cell_side, num_positions, preoccupied_positions, d_cell_list, contact_radius, &vec![-1]);
+            if in_contact == false {break (x, y, z);}
+        };
+        return (x, y, z);
+    }
+    pub fn place_d_cells(mdci: u32, radius: f64, cell_side:f64, num_positions: u32, preoccupied_positions: &mut Vec<Vec<u32>>, d_cell_list: &Vec<DCell>, contact_radius: f64) -> (f64, f64, f64){
+        let mut in_contact: bool = false;
+        let mut d_cell_num: i32 = -1;
+        let mut x: f64; let mut y: f64; let mut z: f64;
+        let (x, y, z) = loop {
+            let (x, y, z) = generate_coords(radius);
+            if outside_sphere(x, y, z, radius) {
+                continue;
+            }
+            let (d_cell_num, in_contact) = check_contact_with_dendrites_d(x, y, z, radius, cell_side, num_positions, preoccupied_positions, d_cell_list, contact_radius);
+            if in_contact == false {break (x, y, z);}
+        };
+        place_dc_on_discrete_grid(x, y, z, mdci, radius, cell_side, preoccupied_positions, num_positions);
+        return (x, y, z);
+    }
+    pub fn place_dc_on_discrete_grid(x: f64, y: f64, z: f64, mdci: u32, radius: f64, cell_side: f64, occupied_positions: &mut Vec<Vec<u32>>, num_positions: u32) {
+        let (coord_x, coord_y, coord_z) = set_coordinates(x, y, z, radius, cell_side);
+        for p in 0..2 {
+            if coord_x + p - 1 >= num_positions as i32{break;}
+            else if coord_x + p - 1 < 0 {continue;}
+            for q in 0..2 {
+                if coord_y + q - 1 >= num_positions as i32{break;}
+                else if coord_y + q - 1 < 0 {continue;}
+                for r in 0..2 {
+                    if coord_z + r - 1 >= num_positions as i32{break;}
+                    else if coord_z + r - 1 < 0 {continue;}
+                    occupied_positions[threed_to_oned(coord_x + p - 1, coord_y + q - 1, coord_z + r - 1, num_positions) as usize].push(mdci);
+                }
+            }
+        }
+
+    }
+    pub fn remove_dc_from_discrete_grid(x: f64, y: f64, z: f64, mdci: u32, radius: f64, cell_side: f64, occupied_positions: &mut Vec<Vec<u32>>, num_positions: u32) {
+        let (coord_x, coord_y, coord_z) = set_coordinates(x, y, z, radius, cell_side);
+        for p in 0..2 {
+            if coord_x + p - 1 >= num_positions as i32{break;}
+            else if coord_x + p - 1 < 0 {continue;}
+            for q in 0..2 {
+                if coord_y + q - 1 >= num_positions as i32{break;}
+                else if coord_y + q - 1 < 0 {continue;}
+                for r in 0..2 {
+                    if coord_z + r - 1 >= num_positions as i32{break;}
+                    else if coord_z + r - 1 < 0 {continue;}
+                    let index = occupied_positions[threed_to_oned(coord_x + p - 1, coord_y + q - 1, coord_z + r - 1, num_positions) as usize].iter().position(|x| *x == mdci).unwrap();
+                    occupied_positions[threed_to_oned(coord_x + p - 1, coord_y + q - 1, coord_z + r - 1, num_positions) as usize].remove(index);
+                }
+            }
+        }
+
+    }
+    pub fn genrand_t_velocity(mean: f64, stdev: f64) -> f64 {
+        //let gamma = Gamma::new(shape, scale);
+        let gamma = Normal::new(mean, stdev);
+        let v = gamma.sample(&mut rand::thread_rng());
+        v
+    }
+    pub fn genrand_uniform_pos() -> f64 {
+        rand::random::<f64>()
+    }
+    pub fn arbitrary_axis_rotation(ax_x: f64, ax_y: f64, ax_z: f64, vec_x: f64, vec_y: f64, vec_z: f64, angle: f64) -> (f64, f64, f64) {
+        let (mut res_x, mut res_y, mut res_z): (f64, f64, f64);
+        let mut rotation_matrix = (angle.cos() + ax_x*ax_x*(1.0 - angle.cos()), ax_x*ax_y*(1.0 - angle.cos()) - ax_z*angle.sin(), ax_x*ax_z*(1.0 - angle.cos()) + ax_y*angle.sin(),
+        ax_x*ax_y*(1.0 - angle.cos()) + ax_z*angle.sin(), angle.cos() + ax_y*ax_y*(1.0 - angle.cos()), ax_y*ax_z*(1.0 - angle.cos()) - ax_x*angle.sin(),
+        ax_x*ax_z*(1.0 - angle.cos()) - ax_y*angle.sin(), ax_y*ax_z*(1.0 - angle.cos()) + ax_x*angle.sin(), angle.cos() + ax_z*ax_z*(1.0 - angle.cos()));
+        res_x = vec_x*&rotation_matrix.0 + vec_y*&rotation_matrix.1 + vec_z*&rotation_matrix.2;
+        res_y = vec_x*&rotation_matrix.3 + vec_y*&rotation_matrix.4 + vec_z*&rotation_matrix.5;
+        res_z = vec_x*&rotation_matrix.6 + vec_y*&rotation_matrix.7 + vec_z*&rotation_matrix.8;
+        return (res_x, res_y, res_z);
+    }
+    pub fn genrand_t_freepath(mean: f64, stdev: f64) -> f64 {
+        let normal = Normal::new(mean, stdev);
+        let v = normal.sample(&mut rand::thread_rng());
+        v
+    }
+    pub fn get_prob_activation(mhc_in_contact: u32, total_mhc: u32, threshold: u32, cog_ag_ratio: f64) -> f64 {
+        let binom = Binomial::new(cog_ag_ratio, mhc_in_contact as u64).unwrap();
+        return 1.0 - binom.cdf((threshold - 1) as f64)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,270 @@
+mod functions;
+use rand::Rng;
+
+// Declaring the constants for the LN model in the global scope. Defining as floats will help with SA later on
+const DEFAULT_RADIUS: f64 = 500.0;
+const DEFAULT_CONTACT_RADIUS: f64 = 20.0;
+const T_CELL_DENSITY: f64 = 1e6;
+const DEFAULT_AGSPEC_FREQ: f64 = 1e-5;
+const TOTAL_TNUM: f64 = DEFAULT_AGSPEC_FREQ*T_CELL_DENSITY*4.0*0.33333*3.1415926*DEFAULT_RADIUS*DEFAULT_RADIUS*DEFAULT_RADIUS*1e-9;
+const T_VELOCITY_MEAN: f64 = 10.06354;
+const T_VELOCITY_STDEV: f64 = 0.5;
+const _T_GAMMA_SHAPE: f64 = 2.28567314561;
+const _T_GAMMA_SCALE: f64 = 4.40287799651;
+const T_FREE_PATH_MEAN: f64 = 25.0;
+const T_FREE_PATH_STDEV: f64 = 3.0;
+const NUM_ANTIGEN_IN_CONTACT_AREA: u32 = 500;
+const NUM_ANTIGEN_ON_DC: u32 = 100000;
+const TCELL_ACTIVATION_THRESHOLD: u32 = 20;
+const FIRST_DC_ARRIVAL: f64 = 1080.0;
+const DC_ARRIVAL_DURATION: f64 = 5.0;//360.0;
+const _NUMBER_DCS: u32 = 720;
+const DC_FLUX: f64 = 0.1; // per minute
+const DEFAULT_ANTIGEN_DECAY_RATE: f64 = 0.001;
+const DEFAULT_COGNATE_RATIO_MEAN_AT_DERMIS: f64 = 0.2;
+const _ANTIGEN_OOB_TOLERANCE: f64 = 0.1*_NUMBER_DCS as f64;
+const _POSITION_OOB_TOLERANCE: f64 = 100.0*_NUMBER_DCS as f64;
+const MINIMUM_CONTACT_GRID_MEMORY_REDUCTION_FACTOR: f64 = 0.25;
+const _TIME_TOLERANCE: f64 = 1e-6;
+
+const NUM_REPEATS: u16 = 2;
+const DEFAULT_TIMESTEP: f64 = 0.05;
+const NUM_TIME_MEASUREMENTS: u32 = 500;
+const NUM_TIMESTEPS_2DAY: f64 = 2880.0 / DEFAULT_TIMESTEP;
+
+// cross-presentation parameters
+
+
+// the main function which coordinates all the activities of the model
+fn main() {
+    return simulation(NUM_TIMESTEPS_2DAY.round() as u32, DEFAULT_TIMESTEP, NUM_TIME_MEASUREMENTS, DEFAULT_RADIUS, DEFAULT_CONTACT_RADIUS, TOTAL_TNUM.round() as u32)
+}
+
+fn simulation(num_time_steps: u32, time_step: f64, _num_time_measurements: u32, radius: f64, contact_radius: f64, num_t_cells: u32) {
+
+    // creating the variables we need
+    let mut total_num_activated: u32 = 0;
+    let mut _total_num_unique_interactions: u32 = 0;
+    let mut total_num_interactions: u32 = 0;
+    let cog_ag_on_arrival: f64 = DEFAULT_COGNATE_RATIO_MEAN_AT_DERMIS*f64::exp(-DEFAULT_ANTIGEN_DECAY_RATE*FIRST_DC_ARRIVAL);
+    let num_d_cells: u32 = (2880.0*DC_FLUX) as u32;
+    
+    // we must first make containers of dCells and tCells
+    let mut d_cell_list = Vec::new();
+    let mut t_cell_list = Vec::new();
+    for i in 0..num_d_cells {
+        let some = functions::functions::DCell {x: -1.0, y : -1.0, z : -1.0, cog_ag_ratio: 0.0, prob_activation: 0.0, time_antigen_count_last_updated: 0.0};
+        d_cell_list.push(some);
+    }
+    for i in 0..num_t_cells {
+        let some = functions::functions::TCell {x: -1.0, y : -1.0, z : -1.0, vx: 0.0, vy: 0.0, vz: 0.0, failed_dc_contact: vec![-1], interactions: 0};
+        t_cell_list.push(some);
+    }
+    
+    // create a discrete grid to search space for T cells looking for DCs. Make this grid iteratively more coarse until the memory usage is less than 1GB
+    let mut occupied_positions_array_size = 100000000;
+    let mut contract_grid_reduction_factor = 2.0;
+    let mut num_positions: u32 = 0;
+
+    while (occupied_positions_array_size*10*4) as f64 > 2e9 {
+        if contract_grid_reduction_factor < MINIMUM_CONTACT_GRID_MEMORY_REDUCTION_FACTOR{
+            return;
+        }
+        else {
+            contract_grid_reduction_factor /= 2.0;
+        }
+        num_positions = (2.0*contract_grid_reduction_factor*radius / contact_radius+1.0) as u32;
+        occupied_positions_array_size = num_positions*num_positions*num_positions;
+    }
+    let cell_side: f64 = contact_radius/contract_grid_reduction_factor;
+
+    // allocating memory
+    let mut occupied_positions: Vec<Vec<u32>> = Vec::with_capacity(occupied_positions_array_size as usize);
+    for i in 0..occupied_positions_array_size {
+        occupied_positions.insert(i as usize, Vec::new())
+    }
+    assert_eq!(occupied_positions.len(), occupied_positions_array_size as usize);
+
+    let mut preoccupied_positions: Vec<Vec<u32>> = Vec::with_capacity(occupied_positions_array_size as usize);
+    for i in 0..occupied_positions_array_size {
+        preoccupied_positions.insert(i as usize, Vec::new())
+    }
+    assert_eq!(occupied_positions.len(), occupied_positions_array_size as usize);
+
+    // more containers for T cells
+    let mut cell_movement_order: Vec<u32> = Vec::with_capacity(num_t_cells as usize);
+    let mut free_path_remaining: Vec<f64> = vec![0.0; num_t_cells as usize];
+    let time_steps_betwee_dc_arrival: f64 = (DC_ARRIVAL_DURATION/time_step)/(num_d_cells - 1) as f64;
+
+    for repeat in 0..NUM_REPEATS {
+        // reinitialise number of activated t-cells and the positions of DCs
+        let mut d_cells_present: u32 = 0;
+        let mut this_num_activated: u32 = 0;
+        let mut _this_num_unique_interactions: u32 = 0;
+        let mut this_num_interactions: u32 = 0;
+        let mut d_travelled_x = vec![0.0; num_t_cells as usize];
+        let mut d_travelled_y = vec![0.0; num_t_cells as usize];
+        let mut d_travelled_z = vec![0.0; num_t_cells as usize];
+
+        if repeat > 0 {
+            for i in 0..occupied_positions_array_size {
+                occupied_positions[i as usize].clear();
+                preoccupied_positions[i as usize].clear();
+            }
+        };
+        cell_movement_order.resize(num_t_cells as usize, 0);
+        for i in 0..num_t_cells {cell_movement_order[i as usize] = i};
+
+        // place D Cells on lattice
+        for i in 0..num_d_cells {
+            let posn  = functions::functions::place_d_cells(i, radius, cell_side, num_positions, &mut preoccupied_positions, &d_cell_list, contact_radius);
+            d_cell_list[i as usize].x = posn.0;
+            d_cell_list[i as usize].y = posn.1;
+            d_cell_list[i as usize].z = posn.2;
+            d_cell_list[i as usize].cog_ag_ratio = cog_ag_on_arrival;
+        }
+        // place T Cells on a lattice
+        for i in 0..num_t_cells {
+            let (xnew, ynew, znew)  = functions::functions::place_t_cells(radius, cell_side, num_positions, &preoccupied_positions, &d_cell_list, contact_radius);
+            t_cell_list[i as usize].x = xnew;
+            t_cell_list[i as usize].y = ynew;
+            t_cell_list[i as usize].z = znew;
+            t_cell_list[i as usize].failed_dc_contact = vec![-1];
+            free_path_remaining[i as usize] = 0.0;
+        }
+
+        for t in 0..num_time_steps {
+
+            //for i in 0..occupied_positions_array_size {occupied_positions[i as usize].clear()};
+            
+            // spawn new DCs
+            // while t as f64*time_step < DC_ARRIVAL_DURATION && t as f64>= d_cells_present as f64*time_steps_betwee_dc_arrival{
+            //     let d_cell_num: u32 = d_cells_present;
+            //     d_cell_list[d_cell_num as usize].time_antigen_count_last_updated = 0.0;
+            //     d_cell_list[d_cell_num as usize].cog_ag_ratio = cog_ag_on_arrival;
+            //     functions::functions::place_dc_on_discrete_grid(d_cell_list[d_cell_num as usize].x, d_cell_list[d_cell_num as usize].y, d_cell_list[d_cell_num as usize].z, d_cell_num, radius, cell_side, &mut occupied_positions, num_positions);
+            //     d_cells_present += 1;
+            // }
+
+            // spawning function for flux model
+            while t as f64*time_step*DC_FLUX >= (d_cells_present % num_d_cells + 1) as f64 { // we haven't got as many DCs as we should, better add one   
+            let dloc = d_cells_present % num_d_cells; // dloc is the position in the d_cell_list we will be adding/removing
+            if d_cells_present > num_d_cells {
+                functions::functions::remove_dc_from_discrete_grid(d_cell_list[dloc as usize].x, d_cell_list[dloc as usize].y, d_cell_list[dloc as usize].z, dloc, radius, cell_side, &mut occupied_positions, num_positions); // get rid of old DC that was here!
+                let posn  = functions::functions::place_d_cells(dloc, radius, cell_side, num_positions, &mut occupied_positions, &d_cell_list, contact_radius);
+                d_cell_list[dloc as usize].x = posn.0;
+                d_cell_list[dloc as usize].y = posn.1;
+                d_cell_list[dloc as usize].z = posn.2;
+            }
+            d_cell_list[dloc as usize].time_antigen_count_last_updated = t as f64 * time_step;
+            d_cell_list[dloc as usize].cog_ag_ratio = cog_ag_on_arrival;
+            functions::functions::place_dc_on_discrete_grid(d_cell_list[dloc as usize].x, d_cell_list[dloc as usize].y, d_cell_list[dloc as usize].z, dloc, radius, cell_side, &mut occupied_positions, num_positions);
+            d_cells_present += 1;
+            }
+
+            // move our t cells and apply boundary conditions
+            let mut pull_backs = 0;
+            //if t%500==0{println!("{}, {}, {}", cell_movement_order[0], cell_movement_order[1], cell_movement_order[2])};
+            for cell in 0..cell_movement_order.len() {
+                let t_cell_num = cell_movement_order[cell - pull_backs];
+                let mut x: f64 = t_cell_list[t_cell_num as usize].x; let mut y: f64 = t_cell_list[t_cell_num as usize].y; let mut z: f64 = t_cell_list[t_cell_num as usize].z;
+                let vx: f64 = t_cell_list[t_cell_num as usize].vx; let vy: f64 = t_cell_list[t_cell_num as usize].vy; let vz: f64 = t_cell_list[t_cell_num as usize].vz;
+                let dx: f64 = vx*time_step; let dy: f64 = vy*time_step; let dz: f64 = vz*time_step;
+                d_travelled_x[t_cell_num as usize] += (dx*dx).sqrt();
+                d_travelled_z[t_cell_num as usize] += (dz*dz).sqrt();
+                d_travelled_y[t_cell_num as usize] += (dy*dy).sqrt();
+                let (mut newx, mut newy, mut newz) = (x + dx, y + dy, z + dz);
+                free_path_remaining[t_cell_num as usize] -= functions::functions::magnitude(dx, dy, dz);
+                
+                // see if we've left sphere or reached end of mean free path
+                if functions::functions::outside_sphere(newx, newy, newz, radius) {
+                    // regenerate velocity to move t cell away from sphere surface
+                    let vmag = functions::functions::genrand_t_velocity(T_VELOCITY_MEAN, T_VELOCITY_STDEV);
+                    let theta = functions::functions::genrand_uniform_pos().acos();
+                    let phi = 6.283185307*functions::functions::genrand_uniform_pos();
+                    let magsq = functions::functions::magnitude(x, y, z);
+                    x /= magsq; y /= magsq; z /= magsq;
+                    let (v0, v1, v2): (f64, f64, f64);
+                    v0 = -vmag*x; v1 = -vmag*y; v2 = -vmag*z;
+                    let (v0, v1, v2) = functions::functions::arbitrary_axis_rotation(z, y, -x, v0, v1, v2, theta);
+                    let (v0, v1, v2) = functions::functions::arbitrary_axis_rotation(x, y, z, v0, v1, v2, phi);
+                    //println!("{}", functions::functions::magnitude(v0, v1, v2));
+                    t_cell_list[t_cell_num as usize].vx = v0; t_cell_list[t_cell_num as usize].vy = v1; t_cell_list[t_cell_num as usize].vz = v2;
+                    free_path_remaining[t_cell_num as usize] = functions::functions::genrand_t_freepath(T_FREE_PATH_MEAN, T_FREE_PATH_STDEV);
+            
+                    // finally, reverse previous step we tried to make and take a new one
+                    newx += v0*time_step - dx; newy += v1*time_step - dy; newz += v2*time_step - dz;
+                }
+                else if free_path_remaining[t_cell_num as usize] <= 0.0 {
+                    let vmag = functions::functions::genrand_t_velocity(T_VELOCITY_MEAN, T_VELOCITY_STDEV);
+                    let theta = functions::functions::genrand_uniform_pos().acos();
+                    let phi = 6.283185307*functions::functions::genrand_uniform_pos();
+                    t_cell_list[t_cell_num as usize].vx = vmag*theta.sin()*phi.cos(); 
+                    t_cell_list[t_cell_num as usize].vy = vmag*theta.sin()*phi.sin(); 
+                    t_cell_list[t_cell_num as usize].vz = vmag*theta.cos();
+                    //println!("{}", functions::functions::magnitude(t_cell_list[t_cell_num as usize].vx, t_cell_list[t_cell_num as usize].vy, t_cell_list[t_cell_num as usize].vz));
+                    //println!("{}, {}, {}", t_cell_list[t_cell_num as usize].vx, t_cell_list[t_cell_num as usize].vy, t_cell_list[t_cell_num as usize].vz);
+                    free_path_remaining[t_cell_num as usize] = functions::functions::genrand_t_freepath(T_FREE_PATH_MEAN, T_FREE_PATH_STDEV);
+                }
+
+                // check for DCs nearby to activate t cell and either update position or remove from simulation
+                let (d_cell_num, in_contact) = functions::functions::check_contact_with_dendrites_t(newx, newy, newz, radius, cell_side, num_positions, &occupied_positions, &d_cell_list, contact_radius, &t_cell_list[t_cell_num as usize].failed_dc_contact);
+                if in_contact {
+                    d_cell_list[d_cell_num as usize].cog_ag_ratio *= f64::exp(-((t as f64 * time_step) - d_cell_list[d_cell_num as usize].time_antigen_count_last_updated) * DEFAULT_ANTIGEN_DECAY_RATE);
+                    let (d_coord_x, d_coord_y, d_coord_z) = functions::functions::set_coordinates(d_cell_list[d_cell_num as usize].x, d_cell_list[d_cell_num as usize].y, d_cell_list[d_cell_num as usize].z, radius, cell_side);
+                    //println!("{}, {}", t_cell_num, d_cell_num);
+                    this_num_interactions += 1;
+                    //println!("Time is {}, Ratio is {}", t as f64*time_step, d_cell_list[d_cell_num as usize].cog_ag_ratio);
+                    if rand::random::<f64>() < functions::functions::get_prob_activation(NUM_ANTIGEN_IN_CONTACT_AREA, NUM_ANTIGEN_ON_DC, TCELL_ACTIVATION_THRESHOLD, d_cell_list[d_cell_num as usize].cog_ag_ratio) {
+                        // activation! We no longer need to track this T cell. Erase it and pull the iterator back one
+                        assert_eq!(cell_movement_order.remove(cell - pull_backs), t_cell_num);
+                        pull_backs += 1; // we have to pull back the iterator so we don't skip any cells
+                        // instead we choose to respawn this t cell somewhere else to keep the density in the paracortex at the same level
+                        //let (xnew, ynew, znew)  = functions::functions::place_t_cells(radius, cell_side, num_positions, &occupied_positions, &d_cell_list, contact_radius);
+                        //t_cell_list[t_cell_num as usize].x = xnew;
+                        //t_cell_list[t_cell_num as usize].y = ynew;
+                        //t_cell_list[t_cell_num as usize].z = znew;
+                        //t_cell_list[t_cell_num as usize].failed_dc_contact = vec![-1];
+                        //free_path_remaining[t_cell_num as usize] = 0.0;
+                        this_num_activated +=1 ;
+                    }
+                    else {
+                        // activation failed - mark this pair so that they won't repeatedly try to interact
+                        t_cell_list[t_cell_num as usize].failed_dc_contact.push(d_cell_num);
+                        // regenerate velocity to move t cell away from DC after interaction
+                        let vmag = functions::functions::genrand_t_velocity(T_VELOCITY_MEAN, T_VELOCITY_STDEV);
+                        let theta = functions::functions::genrand_uniform_pos().acos();
+                        let phi = 6.283185307*functions::functions::genrand_uniform_pos();
+                        x = newx - d_cell_list[d_cell_num as usize].x;
+                        y = newy - d_cell_list[d_cell_num as usize].y;
+                        z = newz - d_cell_list[d_cell_num as usize].z;
+                        let magsq = functions::functions::magnitude(x, y, z);
+                        x /= magsq; y /= magsq; z /= magsq;
+                        let (v0, v1, v2): (f64, f64, f64);
+                        v0 = -vmag*x; v1 = -vmag*y; v2 = -vmag*z;
+                        let (v0, v1, v2) = functions::functions::arbitrary_axis_rotation(z, y, -x, v0, v1, v2, theta);
+                        let (v0, v1, v2) = functions::functions::arbitrary_axis_rotation(x, y, z, v0, v1, v2, phi);
+                        t_cell_list[t_cell_num as usize].vx = v0; t_cell_list[t_cell_num as usize].vy = v1; t_cell_list[t_cell_num as usize].vz = v2;
+                        free_path_remaining[t_cell_num as usize] = functions::functions::genrand_t_freepath(T_FREE_PATH_MEAN, T_FREE_PATH_STDEV);
+                    }
+                }
+                t_cell_list[t_cell_num as usize].x = newx; t_cell_list[t_cell_num as usize].y = newy; t_cell_list[t_cell_num as usize].z = newz; 
+            } // end of t cell loop
+        } // end of time loop
+        total_num_activated += this_num_activated;
+        total_num_interactions += this_num_interactions;
+        println!("{}, {}", this_num_interactions, this_num_activated);
+    } // end of repeat loop
+    println!("Total number of interactions: {}", total_num_interactions);
+    println!("Total number of activations: {}", total_num_activated);
+} // end of function
+// working perfectly with gaussian velocities as of 28/1/21
+
+fn cross_presentation() {
+        
+}
+
+/* TO ADD:_
+[x] flux of DCs arriving -> also need DCs leaving at a floating rate
+[] cross-presentation model -> run once at beginning, should then have arrival values of all 5 peptides
+*/
